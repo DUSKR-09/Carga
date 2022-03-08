2a0343c409f7c16a13aca184370c0bd17a9aa0e2
DUSKR-09 <rogelio.argueta1@gmail.com>
2022-03-08 11:10:23 -0600

    prueba de lectura

--- carga.py
+++ carga.py
@@ -1,6 +1,15 @@
+import pymysql
 import xlrd
-import xlwt
-import os
-import pandas.io.sql as sql
-from cofingparser import CofingParser
-import mysql.connector
+
+book = xlrd.open_workbook('C:\carga\DataPrueba.xlsx')
+sheet = book.sheet_by_index(0)
+
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             db='carga')
+
+cursor = connection.cursor()
+query = """INSERT INTO movimientos (codigo, nombre, apellido1, apellido2, tc, fch_con, monto, saldo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
+
+print(sheet.cell_value(1, 1))
