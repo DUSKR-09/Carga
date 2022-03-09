7f6c712170a3a7863371fbce57e9e72ec02efd8c
DUSKR-09 <rogelio.argueta1@gmail.com>
2022-03-09 14:57:09 -0600

    bug de carga de datos corregido
    Había un error aparentemente en la consulta por números de tarjeta
    pero en realidad era un problema del script de carga de datos
    que guardaba un solo número de tarjeta repetido

--- carga.py
+++ carga.py
@@ -12,18 +12,17 @@ connection = pymysql.connect(host='localhost',
 cursor = connection.cursor()
 query = """INSERT INTO movimientos (codigo, nombre, apellido1, apellido2, tcNum, fchCon, monto, saldo) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"""
 
-""" print(sheet.cell_value(1, 1)) """
+
 for r in range(1, sheet.nrows):
     codigo   = sheet.cell(r,0).value
     nombre   = sheet.cell(r,1).value
     apellido1 = sheet.cell(r,2).value
     apellido2 = sheet.cell(r,3).value
-    tc = sheet.cell(r,4).value
-    fch_con = xlrd.xldate_as_datetime(sheet.cell(r,5).value, book.datemode)
+    numTarjeta = sheet.cell(r,4).value
+    fchCon = xlrd.xldate_as_datetime(sheet.cell(r,5).value, book.datemode)
     monto = sheet.cell(r,6).value
     saldo = sheet.cell(r,7).value
-
-    values = codigo, nombre, apellido1, apellido2, tc, fch_con, monto, saldo
+    values = codigo, nombre, apellido1, apellido2, numTarjeta, fchCon, monto, saldo
     cursor.execute(query, values)
 
 connection.commit()
